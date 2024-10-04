93b35e97fe02d6fcac6c6e707cfa56effe78ffd7
jaeheesong-ktc <jaeheesong-ktc@users.noreply.github.com>
2024-10-04 01:17:06 +0000

    deploy: 956a96a0dc8346fd52073da2a89c79346a3e37da

---
+++ assets/Button.stories-BkhFMMCh.js
@@ -0,0 +1,20 @@
+import{f as z}from"./index-C_FWhylE.js";import{j}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const f=({primary:b=!1,size:B="medium",label:x,...v})=>{const k=b?"ktc-color-text-primary":"ktc-color-text-secondary";return j.jsx("button",{className:["storybook-button",`storybook-button--${B}`,k].join(" "),...v,children:x})};f.__docgenInfo={description:"",methods:[],displayName:"Button",props:{primary:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}},size:{required:!1,tsType:{name:"union",raw:"'small' | 'medium' | 'large'",elements:[{name:"literal",value:"'small'"},{name:"literal",value:"'medium'"},{name:"literal",value:"'large'"}]},description:"",defaultValue:{value:"'medium'",computed:!1}},label:{required:!0,tsType:{name:"string"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const _={title:"Example/Button",component:f,parameters:{layout:"centered"},tags:["autodocs"],args:{onClick:z()}},e={args:{primary:!0,label:"Button"}},r={args:{label:"Button"}},a={args:{size:"small",label:"Button"}},t={args:{size:"large",label:"Button"}};var s,o,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var l,m,u;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(u=(m=r.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var i,c,d;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var p,g,y;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(y=(g=t.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};const E=["primary","secondary","small","medium"];export{E as __namedExportsOrder,_ as default,t as medium,e as primary,r as secondary,a as small};
