3701181acf2ce70a6d651c7c63faf8ce0ea7a164
jaeheesong-ktc <jaeheesong-ktc@users.noreply.github.com>
2024-10-04 01:41:11 +0000

    deploy: 557977122ae0be671b508ff3c63e2fce84ae62d0

--- src/components/button/Button.d.ts
+++ src/components/button/Button.d.ts
@@ -2,7 +2,9 @@ export interface ButtonProps {
     primary?: boolean;
     size?: 'small' | 'medium' | 'large';
     label: string;
-    backgroundColor: string;
     onClick?: () => void;
 }
-export declare const Button: ({ primary, size, label, backgroundColor, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
+/**
+ * Primary UI component for user interaction
+ */
+export declare const Button: ({ primary, size, backgroundColor, label, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
